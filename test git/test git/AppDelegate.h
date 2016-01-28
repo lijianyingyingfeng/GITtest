070033d19fe44a9eafe120222c9ef011d1eb0499
lijianyingyingfeng <1392552158@qq.com>
2016-01-28 18:10:52 +0800

    第一次

---
+++ test git/test git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test git
+//
+//  Created by 123 on 16/1/28.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test git/test git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test git
+//
+//  Created by 123 on 16/1/28.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
